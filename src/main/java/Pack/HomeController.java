dd54a4b2c9a4bfac62394f29fbc55232dc98fee4
Ahmed Khattab <ahmed.khattab20@hotmail.com>
2019-03-04 17:23:29 -0500

    update to fixx travisci
    Attempting to edit index.html to troubleshoot

---
+++ src/main/java/Pack/HomeController.java
@@ -0,0 +1,13 @@
+package Pack;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
