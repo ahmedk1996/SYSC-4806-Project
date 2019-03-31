f5fd6b8ee8457e6242bbcf21498c0f70adaac6ca
Ahmed Khattab <ahmed.khattab20@hotmail.com>
2019-03-31 18:47:46 -0400

    changes

--- src/main/java/Pack/TopicRepository.java
+++ src/main/java/Pack/TopicRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TopicRepository extends CrudRepository<Topic, Integer> {
-
     List<Topic> findByDescription(String description);
     List<Topic> findByProfID(int profID);
 }
