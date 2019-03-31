9922e58911c3ddaa1131a38210a3d3ab2e5d5481
Aly Khan Barolia <ak.barolia@hotmail.com>
2019-03-31 16:58:03 -0400

    another attempt to resolve the conflict

--- src/main/java/Pack/ProfController.java
+++ src/main/java/Pack/ProfController.java
@@ -31,11 +31,10 @@ public class ProfController {
     @PostMapping("/add")
     public String addSubmit(@ModelAttribute Topic topic, Model model) {
         topicRepository.save(topic);
+        model.addAttribute("topic", topicRepository.findAll());
         System.out.println("***************" + topic.getPreference1());
         System.out.println("***************" + topic.getPreference2());
         System.out.println("***************" + topic.getPreference3());
-       // model.addAttribute("topic", topic);
-        model.addAttribute("topic", topicRepository.findAll());
         return "Prof";
     }
 
