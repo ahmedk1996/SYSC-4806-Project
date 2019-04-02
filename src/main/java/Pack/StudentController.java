7855667ba91f814f5728eed8a7c2d0e950801585
Hun <hunhoha@cmail.carleton.ca>
2019-04-02 19:55:45 -0400

    OMG
    OMG

--- src/main/java/Pack/StudentController.java
+++ src/main/java/Pack/StudentController.java
@@ -3,13 +3,7 @@ package Pack;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.websocket.server.PathParam;
-
+import org.springframework.web.bind.annotation.*;
 @Controller
 
 public class StudentController {
@@ -34,22 +28,17 @@ public class StudentController {
         return "StudentHome";
     }
 
-    @PostMapping("/detail")
+    @GetMapping("/detail")
     public String detailTopic(){
+
         return "ProjectDetail";
     }
 
-
-//    @GetMapping
-//    public String getList (Model model){
-//       model.addAttribute("topic", topicRepository.findAll());
-//       return "StudentHome";
-//    }
-
-
-
-
-
+    @GetMapping("/ProjectDetail")
+    public String ProjectDetail(@RequestAttribute Topic topic,Model model){
+        model.addAttribute("topicdetail",topic);
+        return "ProjectDetail";
+    }
 
 
 }
