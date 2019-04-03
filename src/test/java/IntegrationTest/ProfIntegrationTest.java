deceb281ec1bdae36af2ff2c5ba750b031a845ee
JTudor <jeff.a.tudor@gmail.com>
2019-04-02 21:49:42 -0400

    Added integration tests for the new mappings

--- src/test/java/IntegrationTest/ProfIntegrationTest.java
+++ src/test/java/IntegrationTest/ProfIntegrationTest.java
@@ -33,6 +33,15 @@ public class ProfIntegrationTest {
     public void testProfPage() throws Exception{
         this.mockMVC.perform(get("/prof")).andExpect(status().isOk()).andExpect(view().name("Prof"));
     }
+    public void testProfDelete() throws Exception{
+        this.mockMVC.perform(get("/delete")).andExpect(status().isOk()).andExpect(view().name("Prof"));
+    }
+    public void testProfArchive() throws Exception{
+        this.mockMVC.perform(get("/archive")).andExpect(status().isOk()).andExpect(view().name("Prof"));
+    }
+    public void testProfActivate() throws Exception{
+        this.mockMVC.perform(get("/activate")).andExpect(status().isOk()).andExpect(view().name("Prof"));
+    }
     public void testProfAddTopicPage() throws Exception{
         this.mockMVC.perform(get("/add")).andExpect(status().isOk()).andExpect(view().name("Addform"));
     }
